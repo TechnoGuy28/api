0e6ea061d6564606edd81ecaab25cb637450e335
Celisson Tulio <celissontulio@gmail.com>
2026-08-28 14:13:43 -0300

    Autenticação

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import { authMiddleware } from '../middlewares/auth.js';
 import * as ctrl from '../controllers/authController.js';
 
 const router = express.Router();
 router.post('/login', ctrl.login);
-router.get('/me', ctrl.me);
-router.post('/logout', ctrl.logout);
+router.get('/me', authMiddleware, ctrl.me);
+router.post('/logout', authMiddleware, ctrl.logout);
 export default router;
